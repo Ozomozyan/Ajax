f047cc964edf60fc661ef41cc2d9fba8d3423598
Ozomozyan <125055262+Ozomozyan@users.noreply.github.com>
2023-05-03 11:28:19 +0200

    Create

---
+++ traitement.php
@@ -0,0 +1,36 @@
+<?php
+$NAME = $_GET['nom'];
+
+function isPalindrome($string) {
+    $string = strtolower($string);
+    $reverse = strrev($string);
+    return $string == $reverse;
+}
+
+// Connect to the database
+$base = new PDO('mysql:host=localhost; dbname=id20172939_ajax', 'id20172939_id20172939_root', 'C>3Gmt-4_2h3Fp)/');
+$base->exec("SET CHARACTER SET utf8");
+
+// Check if the name exists in the database
+$query = $base->prepare("SELECT * FROM Ajax WHERE name = :name");
+$query->bindParam(':name', $NAME);
+$query->execute();
+
+$result = array();
+
+if (isPalindrome($NAME)) {
+    $result['palindrome'] = 'Le mot '.$NAME.' est un palindrome.';
+} else {
+    $result['palindrome'] = 'Le mot '.$NAME.' n\'est pas un palindrome.';
+}
+
+if ($query->rowCount() > 0) {
+    $result['status'] = 'success';
+    $result['message'] = 'Votre nom est présent dans la BDD.';
+} else {
+    $result['status'] = 'error';
+    $result['message'] = 'Nom NON présent dans la BDD.';
+}
+
+echo json_encode($result);
+?>
